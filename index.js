cf4cc165795c6c23d3b8cb069697b0b8738b45a6
Marius Riedl <marius.riedl99@gmail.com>
2022-08-07 20:35:09 +0200

    Add express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+  res.redirect("pages/home/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on :8080");
+});
