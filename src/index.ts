6e33e068e33ff516710a91465802ba0de9062e54
Marius Riedl <marius.riedl99@gmail.com>
2022-08-07 21:39:07 +0200

    Fix static files

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("static"));
+app.use(express.static("src/static"));
 
 app.get("/", (req, res) => {
   res.redirect("pages/home/index.html");
 });
 
 app.listen(PORT, () => {
-  console.log("Listening on :8080");
+  console.log(`Listening on :${PORT}`);
 });
